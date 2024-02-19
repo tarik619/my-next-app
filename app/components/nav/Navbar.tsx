14cb9211e886c55966c25c2f5779d6b4975da6ff
tarik619 <itarik707@gmail.com>
2024-02-19 12:21:03 +0600

    creating navbar

--- app/components/nav/Navbar.tsx
+++ app/components/nav/Navbar.tsx
@@ -1,3 +1,29 @@
+import Link from "next/link";
+import Container from "../Container";
+import { Redressed } from "next/font/google";
+
+const redressed = Redressed({ subsets: ["latin"], weight: ["400"] });
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex items-center justify-between gap-3 md:gap-0">
+            <Link
+              className={`${redressed.className} text-2xl font-bold`}
+              href="/"
+            >
+              E-Shop
+            </Link>
+            <div className="hidden md:block">Search</div>
+            <div className="flex items-center gap-8 md:gap-12">
+              <div className="">CartCount</div>
+              <div className="">usermenu</div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import Navbar from "./components/nav/Navbar";
-import Fotter from "./components/footer/Fotter";
+import Footer from "./components/footer/Fotter";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 
@@ -22,7 +22,7 @@ export default function RootLayout({
         <div className="flex flex-col min-h-screen">
           <Navbar />
           <main className="flex-grow ">{children}</main>
-          <Fotter />
+          <Footer />
         </div>
       </body>
     </html>
