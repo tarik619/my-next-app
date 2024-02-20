c014f5e2ecdcd440f9517e6180e46ea64160f630
tarik619 <itarik707@gmail.com>
2024-02-21 00:27:57 +0600

    productimage component for productdetails page

--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Button from "@/app/components/Button";
+import ProductImages from "@/app/components/products/ProductImages";
 import SetColor from "@/app/components/products/SetColor";
 import SetQuantity from "@/app/components/products/SetQuantity";
 import { Rating } from "@mui/material";
@@ -73,7 +74,11 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2  gap-12">
-      <div className=""></div>
+      <ProductImages
+        cartProduct={cartProduct}
+        product={product}
+        handleColorSelect={handleColorSelect}
+      />
       <div className="flex flex-col gap-1 text-slate-500 text-sm">
         <h2 className="text-3xl font-medium text-slate-700">{product.name}</h2>
         <div className="flex items-center gap-2">
