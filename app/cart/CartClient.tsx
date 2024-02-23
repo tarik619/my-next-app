b88aeeea7b974927362f16ce996bcc973905abce
tarik619 <itarik707@gmail.com>
2024-02-23 21:07:54 +0600

    create layout of our cart page

---
+++ app/cart/CartClient.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { useCart } from "@/hooks/useCart";
+import Link from "next/link";
+import { MdArrowBack } from "react-icons/md";
+import Heading from "../components/products/Heading";
+import Button from "../components/Button";
+
+const CartClient = () => {
+  const { cartProducts } = useCart();
+  if (!cartProducts || cartProducts.length === 0) {
+    return (
+      <div className="flex flex-col items-center ">
+        <div className="text-2xl">Your Cart is empty</div>
+        <div className="">
+          <Link
+            href={"/"}
+            className="text-slate-500 flex items-center gap-1 mt-2"
+          >
+            <MdArrowBack />
+            <span>Start Shopping</span>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="">
+      <Heading title="Shopping Cart" center />
+      <div className="grid grid-cols-5 text-xs gap-4 pb-2 items-center mt-8">
+        <div className="col-span-2 justify-self-start">Product</div>
+        <div className="justify-self-center">Price</div>
+        <div className="justify-self-center">Quantity</div>
+        <div className="justify-self-end">Total</div>
+      </div>
+      <div className="">
+        {cartProducts &&
+          cartProducts.map((item) => (
+            <div className="" key={item.id}>
+              {item.name}
+            </div>
+          ))}
+      </div>
+      <div className="border-t-[1.5px] border-slate-200 py-4 flex justify-between gap-4">
+        <div className="w-[90px]">
+          <Button label="Clear Cart" onClick={() => {}} small outline />
+        </div>
+        <div className="text-sm flex flex-col gap-1 items-start">
+          <div className="flex justify-between w-full text-base font-semibold">
+            <span>Subtotal</span>
+            <span>$1,000</span>
+          </div>
+          <p className="text-slate-500">
+            Taxes and Shipping calculate at checkout
+          </p>
+          <Button label="CheckOut" onClick={() => {}} />
+
+          <Link
+            href={"/"}
+            className="text-slate-500 flex items-center gap-1 mt-2"
+          >
+            <MdArrowBack />
+            <span>Continue Shopping</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartClient;
--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -26,7 +26,7 @@ const Button: React.FC<ButtonProps> = ({
     <button
       onClick={onClick}
       disabled={disabled}
-      className={`disabled:opacity-70 disabled:cursor-not-allowed rounded-md hover:opacity-80 transition w-full border-slate-700 flex items-center justify-center gap-2 ${
+      className={`disabled:opacity-70 disabled:cursor-not-allowed rounded-md hover:opacity-80 transition w-full border-slate-700 border flex items-center justify-center gap-2 ${
         outline ? "bg-white" : "bg-slate-700"
       } ${outline ? "text-slate-700" : "text-white"}
       ${small ? "text-sm font-light" : "text-base"} ${
