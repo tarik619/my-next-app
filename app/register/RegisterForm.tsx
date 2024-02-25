0042a72a02705730816990844fe048dee1975946
tarik619 <itarik707@gmail.com>
2024-02-25 20:03:51 +0600

    creating log in functionality with the help of react-hook-form and creating components form wrap, input,page,registerform

---
+++ app/register/RegisterForm.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { useState } from "react";
+import Input from "../components/inputs/Input";
+import Heading from "../components/products/Heading";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import Button from "../components/Button";
+import Link from "next/link";
+
+const RegisterForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      email: "",
+      password: "",
+      name: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    console.log(data);
+  };
+  return (
+    <>
+      <Heading title="Sign up for E-Shop" />
+      <hr className="bg-slate-300 w-full h-px" />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+        type="password"
+      />
+      <Button
+        label={isLoading ? "Loading" : "Sign Up"}
+        onClick={handleSubmit(onSubmit)}
+      ></Button>
+      <p className="text-sm">
+        Already have an account?{" "}
+        <Link href={"/login"} className="underline">
+          Log In
+        </Link>
+      </p>
+    </>
+  );
+};
+
+export default RegisterForm;
