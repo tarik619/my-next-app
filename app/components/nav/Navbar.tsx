03ceb9513c28f19f4200c112677a764ec7192fc8
tarik619 <itarik707@gmail.com>
2024-02-28 11:22:22 +0600

    creating usemenu component in nav

--- app/components/nav/Navbar.tsx
+++ app/components/nav/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Container from "../Container";
 import { Redressed } from "next/font/google";
 import CartCount from "./CartCount";
+import UserMenu from "./UserMenu";
 
 const redressed = Redressed({ subsets: ["latin"], weight: ["400"] });
 
@@ -20,7 +21,7 @@ export default function Navbar() {
             <div className="hidden md:block">Search</div>
             <div className="flex items-center gap-8 md:gap-12">
               <CartCount />
-              <div className="">usermenu</div>
+              <UserMenu />
             </div>
           </div>
         </Container>
