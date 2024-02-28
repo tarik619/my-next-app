e2240dd14fa2ad24743fff72579ba5b420896a8c
tarik619 <itarik707@gmail.com>
2024-02-28 20:27:08 +0600

    getcurrentuser functionality creating, creating type files to create type of safeuser

--- app/components/nav/Navbar.tsx
+++ app/components/nav/Navbar.tsx
@@ -3,10 +3,12 @@ import Container from "../Container";
 import { Redressed } from "next/font/google";
 import CartCount from "./CartCount";
 import UserMenu from "./UserMenu";
+import { getCurrentUser } from "@/actions/getCurrentUser";
 
 const redressed = Redressed({ subsets: ["latin"], weight: ["400"] });
 
-export default function Navbar() {
+export default async function Navbar() {
+  const currentUser = await getCurrentUser();
   return (
     <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-sm">
       <div className="py-4 border-b-[1px]">
@@ -21,7 +23,7 @@ export default function Navbar() {
             <div className="hidden md:block">Search</div>
             <div className="flex items-center gap-8 md:gap-12">
               <CartCount />
-              <UserMenu />
+              <UserMenu currentUser={currentUser} />
             </div>
           </div>
         </Container>
--- app/components/nav/UserMenu.tsx
+++ app/components/nav/UserMenu.tsx
@@ -7,12 +7,20 @@ import Link from "next/link";
 import MenuItem from "./MenuItem";
 import { signOut } from "next-auth/react";
 import BackDrop from "./BackDrop";
+import { User } from "@prisma/client";
+import { SafeUser } from "@/types";
 
-const UserMenu = () => {
+interface UserMenuProps {
+  currentUser: SafeUser | null;
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleOpen = useCallback(() => {
     setIsOpen((prev) => !prev);
   }, []);
+  console.log(currentUser);
+
   return (
     <>
       <div className="relative z-30 ">
@@ -25,30 +33,33 @@ const UserMenu = () => {
         </div>
         {isOpen && (
           <div className="absolute rounded-md shadow-md w-[170px] bg-white right-0 top-12 text-sm flex flex-col cursor-pointer">
-            <div className="">
-              <Link href={"/orders"}>
-                <MenuItem onClick={toggleOpen}>Your Orders</MenuItem>
-              </Link>
-              <Link href={"/admin"}>
-                <MenuItem onClick={toggleOpen}>Admin Dashboard</MenuItem>
-              </Link>
-              <MenuItem
-                onClick={() => {
-                  toggleOpen();
-                  signOut();
-                }}
-              >
-                Logout
-              </MenuItem>
-            </div>
-            <div className="">
-              <Link href={"/login"}>
-                <MenuItem onClick={toggleOpen}>Log In</MenuItem>
-              </Link>
-              <Link href={"/register"}>
-                <MenuItem onClick={toggleOpen}>Register</MenuItem>
-              </Link>
-            </div>
+            {currentUser ? (
+              <div className="">
+                <Link href={"/orders"}>
+                  <MenuItem onClick={toggleOpen}>Your Orders</MenuItem>
+                </Link>
+                <Link href={"/admin"}>
+                  <MenuItem onClick={toggleOpen}>Admin Dashboard</MenuItem>
+                </Link>
+                <MenuItem
+                  onClick={() => {
+                    toggleOpen();
+                    signOut();
+                  }}
+                >
+                  Logout
+                </MenuItem>
+              </div>
+            ) : (
+              <div className="">
+                <Link href={"/login"}>
+                  <MenuItem onClick={toggleOpen}>Log In</MenuItem>
+                </Link>
+                <Link href={"/register"}>
+                  <MenuItem onClick={toggleOpen}>Register</MenuItem>
+                </Link>
+              </div>
+            )}
           </div>
         )}
       </div>
