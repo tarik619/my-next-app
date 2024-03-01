df55c42aa5b498ef65f6405eaa22786b468af75d
tarik619 <itarik707@gmail.com>
2024-03-01 23:32:32 +0600

    installing firebase for storing images of products and created model for product

---
+++ libs/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBY3GnIOG_6FjXpSmtJ7l1G-H3k9ILhcl4",
+  authDomain: "e-shop-72a6b.firebaseapp.com",
+  projectId: "e-shop-72a6b",
+  storageBucket: "e-shop-72a6b.appspot.com",
+  messagingSenderId: "742561390413",
+  appId: "1:742561390413:web:a1f306d68bf90a7b246104",
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
