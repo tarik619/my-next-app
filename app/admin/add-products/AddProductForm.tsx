d8b30a28353ebc122d983d45dcd832c444dd3aa8
tarik619 <itarik707@gmail.com>
2024-03-02 10:44:30 +0600

    setting up the add product page with components textarea and nulldata if user is not admin

---
+++ app/admin/add-products/AddProductForm.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import Input from "@/app/components/inputs/Input";
+import TextArea from "@/app/components/inputs/TextArea";
+import Heading from "@/app/components/products/Heading";
+import { useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+
+const AddProductForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      description: "",
+      brand: "",
+      category: "",
+      inStock: false,
+      images: [],
+      price: "",
+    },
+  });
+
+  return (
+    <>
+      <Heading title="Add a Product" center />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="price"
+        label="Price"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        type="number"
+        required
+      />
+      <Input
+        id="brand"
+        label="Brand"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <TextArea
+        id="description"
+        label="Description"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </>
+  );
+};
+
+export default AddProductForm;
--- app/admin/add-products/page.tsx
+++ app/admin/add-products/page.tsx
@@ -1,5 +1,25 @@
-const AddProducts = () => {
-  return <div className="">addprodducts</div>;
+import Container from "@/app/components/Container";
+import FormWrap from "@/app/components/FormWrap";
+import AddProductForm from "./AddProductForm";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import NullData from "@/app/components/NullData";
+
+const AddProducts = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return <NullData title="OOOPS! access denied" />;
+  }
+  //
+  return (
+    <div className="">
+      <Container>
+        <FormWrap>
+          <AddProductForm />
+        </FormWrap>
+      </Container>
+    </div>
+  );
 };
 
 export default AddProducts;
