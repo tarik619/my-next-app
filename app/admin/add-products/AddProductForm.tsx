ee733eb95fde0602575f3f7f187a84e7cb4e9781
tarik619 <itarik707@gmail.com>
2024-03-02 11:30:16 +0600

    adding customcheckbox on add product page

--- app/admin/add-products/AddProductForm.tsx
+++ app/admin/add-products/AddProductForm.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import CategoryInput from "@/app/components/inputs/CategoryInput";
+import CustomCheckBox from "@/app/components/inputs/CustomCheckbox";
 import Input from "@/app/components/inputs/Input";
 import TextArea from "@/app/components/inputs/TextArea";
 import Heading from "@/app/components/products/Heading";
+import { categories } from "@/app/utils/Categories";
 import { useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
@@ -27,6 +30,16 @@ const AddProductForm = () => {
       price: "",
     },
   });
+
+  const category = watch("category");
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldValidate: true,
+      shouldDirty: true,
+      shouldTouch: true,
+    });
+  };
 
   return (
     <>
@@ -64,6 +77,31 @@ const AddProductForm = () => {
         errors={errors}
         required
       />
+      <CustomCheckBox
+        id="inStock"
+        register={register}
+        label="This Product is in stock"
+      />
+      <div className="w-full font-medium ">
+        <div className="mb-2 font-semibold">Select categories</div>
+        <div className="grid grid-cols-2 md:grid-cols-3 max-h-[50vh] overflow-y-auto gap-3">
+          {categories.map((item) => {
+            if (item.label === "All") {
+              return null;
+            }
+            return (
+              <div key={item.label} className="col-span">
+                <CategoryInput
+                  onClick={(category) => setCustomValue("category", category)}
+                  selected={category === item.label}
+                  label={item.label}
+                  icon={item.icon}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 };
