ac62a1ab704ad5bbf606bc5405e43ae3d3aaf184
tarik619 <itarik707@gmail.com>
2024-03-05 12:35:40 +0600

    successfully add category section in homepage

--- app/components/nav/Navbar.tsx
+++ app/components/nav/Navbar.tsx
@@ -4,6 +4,7 @@ import { Redressed } from "next/font/google";
 import CartCount from "./CartCount";
 import UserMenu from "./UserMenu";
 import { getCurrentUser } from "@/actions/getCurrentUser";
+import Categories from "./Categories";
 
 const redressed = Redressed({ subsets: ["latin"], weight: ["400"] });
 
@@ -28,6 +29,7 @@ export default async function Navbar() {
           </div>
         </Container>
       </div>
+      <Categories />
     </div>
   );
 }
